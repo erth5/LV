5427057afaab3c52b45c253f7575fcbed63dcb4f
Eggo5 <Rthem@gmx.de>
2022-02-28 17:28:30 +0100

    color system fix

---
+++ public-table/resources/views/sites/chain.blade.php
@@ -0,0 +1,3 @@
+<div class="center">
+    <h1>This is the public Chain-Table number one</h1>
+</div>
--- public-table/resources/views/sites/public.blade.php
+++ public-table/resources/views/sites/public.blade.php
@@ -3,55 +3,13 @@
 @section('title', 'Table')
 @section('content')
 
-<?php /*
-    $obj = array("Oone", "Osec", "Othird")
-    foreach ($obj as $e) {
-        echo $e
-    }
-
-    $obj = array("Oone", "Osec", "Othird")
-    @foreach ($obj as $e)
-        echo $obj
-    @endforeach
-*/?>
-
-<div class="center">
-    <h1>This is public free data table number one</h1>
-</div>
+    <div class="center">
+        <h1>This is the public Table number one</h1>
+    </div>
 
 <!-- Project tabeler static -->
 <input type="submit" value="Submit" name="submit_button">
-<table>
-        <tr>
-            <th>Date</th>
-            <th>User(auto)</th>
-            <th>Word1</th>
-            <th>Word2</th>
-            <th>Word3</th>
-        </tr>
-        <tr>
-            <td>{{$openingText= "hello"}}</td>
-            <td>
-                <form action="/action_page.php"><input type="text" id="?" name="?"></form>
-            </td>
-            <td>
-                <form><input type="text" id="fname" name="fname" placeholder="your text"></form>
-            </td>
-            <td>
-                <form><input type="text" id="fname" name="fname"></form>
-            </td>
-            <td>
-                <form><input type="text" id="fname" name="fname"></form>
-            </td>
-        </tr>
-        <tr>
-            <td>{{$openingText}}</td>
-            <td>{{$openingText}}</td>
-            <td>{{$openingText}}</td>
-            <td>{{$openingText}}</td>
-            <td>{{$openingText}}</td>
-        </tr>
-</table>
+
 
 <table>
         <tr>
