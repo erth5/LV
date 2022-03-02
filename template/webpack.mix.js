4383eb569e0bdf6ec840f7c42ebfc4929e585243
Eggo5 <Rthem@gmx.de>
2022-03-02 13:35:29 +0100

    tailwind config (missing vue)

--- template/webpack.mix.js
+++ template/webpack.mix.js
@@ -11,19 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+// Sass and Css config
 mix.sass('resources/sass/app.sass', 'public/css')
     .sourceMaps();
 
 mix.ts('resources/ts/app.ts', 'public/js')
     .sourceMaps();
 
-// Tailwind expanded without sass
-/*
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss')
-    ]);
-*/
-
-// sync without reload, load localhost:3000
-//mix.browserSync('http://127.0.0.1:8000/');
--- wind/webpack.mix.js
+++ wind/webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+// Tailwind expanded without sass
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss')
     ]);
+
+// sync without reload, load localhost:3000
+//mix.browserSync('http://127.0.0.1:8000/');
